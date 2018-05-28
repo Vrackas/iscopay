26a74bef0f347a41aac7bb93f015c624b4d62775
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-05-28 17:49:07 +0300

    add about pages with responsive

--- app/templates/about/about.controller.js
+++ app/templates/about/about.controller.js
@@ -9,6 +9,29 @@
 
     function AboutController() {
         var vm = this;
+        vm.values = [
+            {
+                image:'content/img/about/1 (1).svg',
+                title:'Опыт',
+                text:'команды, превосходящий ожидания клиентов'
+            },
+            {
+                image:'content/img/about/2 (1).svg',
+                title:'Гибкость',
+                text:'решений для удовлетворения требований клиентов'
+            },
+            {
+                image:'content/img/about/3 (1).svg',
+                title:'Инновации',
+                text:'и лидерство в сфере платёжных технологий'
+            },
+            {
+                image:'content/img/about/4 (1).svg',
+                title:'Доверие',
+                text:'клиентов, как ключевой принцип нашей деятельности'
+            }
+        ]
 
     }
 })();
+
--- app/templates/e-commerce/e-commerce.controller.js
+++ app/templates/e-commerce/e-commerce.controller.js
@@ -22,19 +22,20 @@
                 link: 'merchant_acquiring',
                 text1: 'Get on board our flexible and modular Acquiring Solution and manage your online payments more efficiently.',
                 text2: 'Comprehensive and flexible acquiring solution that delivers broad functionality tailored to iPSPs` e-commerce needs. Our modular solution can be customised and adapted to each individual customer and delivered via scalable API.'
-            },{
-                subTitle: 'iscopay stream',
-                title: 'Payment Processing',
-                link: 'payment_processing',
-                text1: 'Empower your business with our comprehensive Payment Processing Solution for issuers and acquirers.',
-                text2: 'Optimised and adaptable payment processing solution for issuers and acquirers. Our innovative suite of services and advanced technologies offers secure end-to-end authorisation platform to help you successfully compete in today’s highly competitive financial environment. '
-            },{
-                subTitle: 'iscopay smart',
-                title: 'Card Issuing',
-                link: 'card_issuing',
-                text1: 'Expand your offering with our white-label Card Issuing Solution and launch your own card programme.',
-                text2: 'White-label solution for branded card issuing and management. Our advanced card issuing solution provides excellent opportunities to increase market competitiveness and launch personalised card programmes quickly and efficiently.'
-            }
+            },
+            // {
+            //     subTitle: 'iscopay stream',
+            //     title: 'Payment Processing',
+            //     link: 'payment_processing',
+            //     text1: 'Empower your business with our comprehensive Payment Processing Solution for issuers and acquirers.',
+            //     text2: 'Optimised and adaptable payment processing solution for issuers and acquirers. Our innovative suite of services and advanced technologies offers secure end-to-end authorisation platform to help you successfully compete in today’s highly competitive financial environment. '
+            // },{
+            //     subTitle: 'iscopay smart',
+            //     title: 'Card Issuing',
+            //     link: 'card_issuing',
+            //     text1: 'Expand your offering with our white-label Card Issuing Solution and launch your own card programme.',
+            //     text2: 'White-label solution for branded card issuing and management. Our advanced card issuing solution provides excellent opportunities to increase market competitiveness and launch personalised card programmes quickly and efficiently.'
+            // }
         ]
 
 
