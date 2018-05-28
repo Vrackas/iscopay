86112c424fe5866c20885f68503add4c121d0117
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-05-28 21:19:13 +0300

    add contact pages with responsive

--- app/templates/contact/contact.controller.js
+++ app/templates/contact/contact.controller.js
@@ -10,5 +10,25 @@
     function ContactController() {
         var vm = this;
 
+        vm.items =[
+            {
+                title:'Интернет-эквайринг',
+                contact:'merchants@decta.com'
+            },
+            {
+                title:'Процессинг платежей',
+                contact:'pro@decta.com'
+            },
+            {
+                title:'Эмиссия карт',
+                contact:'cards@decta.com'
+            },
+            {
+                title:'Партнерская программа',
+                contact:'partners@decta.com'
+            }
+        ]
     }
+
+
 })();
