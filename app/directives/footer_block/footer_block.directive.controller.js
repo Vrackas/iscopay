ef735bb17449c2dc9d17f4da229b80320e04f7d8
kravcovvalentin@gmail.com <kravcovvalentin@gmail.com>
2018-06-06 14:52:34 +0300

    add privacy pdf

--- app/directives/footer_block/footer_block.directive.controller.js
+++ app/directives/footer_block/footer_block.directive.controller.js
@@ -11,5 +11,17 @@
     function FooterBlockController($state) {
         let vm = this;
 
+        vm.privacyOpen=privacyOpen;
+
+        function privacyOpen(){
+            var lang = navigator.language;
+            if (lang.indexOf('ru') !== -1 || lang.indexOf('kz') !== -1) {
+                location.href = "../../content/pdf/privacy_ru.pdf";
+            }
+            else {
+                location.href = "../../content/pdf/privacy_eu.pdf";
+            }
+        }
+
     }
 })();
